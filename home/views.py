78b70cf9974c60cf7ec5700a5d445a3598e88cbf
betulsari_97@hotmail.com <betulsari_97@hotmail.com>
2021-04-29 01:47:06 +0300

    iletişim formu aktifleşti

--- home/views.py
+++ home/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
-
+from home.models import Setting, ContactFormu, ContactFormMessage
 
 def index(request ):
     setting = Setting.objects.get(pk=1)
@@ -20,7 +20,22 @@ def referanslar(request ):
     context = {'setting' : setting}
     return render(request, 'referanslarimiz.html', context)
 
-def iletisim(request ):
+def iletisim(request):
+
+    if request.method == 'POST':
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip= request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request,"mesajınız başarı ile gönderilmiştir. Teşekkür ederiz.")
+            return HttpResponseRedirect ('/iletisim')
+
     setting = Setting.objects.get(pk=1)
-    context = {'setting' : setting, 'page':'hakkimizda'}
+    form = ContactFormu()
+    context = {'setting': setting, 'form':form}
     return render(request, 'iletisim.html', context)
